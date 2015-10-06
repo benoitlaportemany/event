82ed2654d5f1b175954abef9258ec2e27c3169b6
Poudr <pierrekfoury78@hotmail.fr>
2015-10-06 16:20:47 +0200

    recommit

--- index.php
+++ index.php
@@ -15,6 +15,13 @@
 
 
 
+<div class="example">
+  <div class="col-lg-4">
+    <div class="input-group">
+      <span class="input-group-btn">
+      </span>
+      <input type="text" class="form-control" placeholder="Search for...">
+    </div><!-- /input-group -->
 
   
     <div class="input-group">
